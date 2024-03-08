a06e214ce69187e88c5a46aca870c15c5aea83c4
Alireza67 <Alirezaabbasim@gmail.com>
2024-03-08 10:48:22 +0330

    [UPD] udp endpoints

--- basics/endpoint.cpp
+++ basics/endpoint.cpp
@@ -9,9 +9,10 @@
 using namespace boost;
 using namespace std::literals;
 
-using EndPoint = asio::ip::tcp::endpoint;
+using TcpEndPoint = asio::ip::tcp::endpoint;
+using UdpEndPoint = asio::ip::udp::endpoint;
 
-EndPoint CreateEndpoing(std::string& ip, unsigned short port)
+TcpEndPoint CreateEndpoing(std::string& ip, unsigned short port)
 {
 	boost::system::error_code ec;
 	asio::ip::address ip_address = asio::ip::address::from_string(ip, ec);
@@ -23,14 +24,14 @@ EndPoint CreateEndpoing(std::string& ip, unsigned short port)
 		throw std::runtime_error(msg.str());
 	}
 
-	return EndPoint(ip_address, port);
+	return TcpEndPoint(ip_address, port);
 }
 
-EndPoint CreateEndpoing(unsigned short port)
+TcpEndPoint CreateEndpoing(unsigned short port)
 {
 	boost::system::error_code ec;
 	asio::ip::address ip_address = asio::ip::address_v4::any();
-	return EndPoint(ip_address, port);
+	return TcpEndPoint(ip_address, port);
 }
 
 TEST(endpoint, creating_client_endpoint) 
@@ -67,6 +68,49 @@ TEST(endpoint, creating_serverside_endpoint)
 	auto ip = "0.0.0.0"s;
 	unsigned short port = 6969;
 	auto ep = CreateEndpoing(port);
+
+	EXPECT_EQ(port, ep.port());
+	EXPECT_EQ(ip, ep.address().to_string());
+}
+
+UdpEndPoint CreateEndpoingUdp(std::string& ip, unsigned short port)
+{
+	boost::system::error_code ec;
+	asio::ip::address ip_address = asio::ip::address::from_string(ip, ec);
+	if (ec.value() != 0)
+	{
+		std::stringstream msg;
+		msg << "Failed to parse the IP address. Error code = "
+			<< ec.value() << ". Message: " << ec.message();
+		throw std::runtime_error(msg.str());
+	}
+
+	return UdpEndPoint(ip_address, port);
+}
+
+UdpEndPoint CreateEndpoingUdp(unsigned short port)
+{
+	boost::system::error_code ec;
+	asio::ip::address ip_address = asio::ip::address_v6::any();
+	return UdpEndPoint(ip_address, port);
+}
+
+TEST(endpoint, creating_udp_client_endpoint)
+{
+	auto ip = "127.0.0.1"s;
+	unsigned short port = 6969;
+	auto ep = CreateEndpoingUdp(ip, port);
+
+	EXPECT_EQ(port, ep.port());
+	EXPECT_EQ(ip, ep.address().to_string());
+	EXPECT_EQ(typeid(asio::ip::udp).name(), typeid(ep.protocol()).name());
+}
+
+TEST(endpoint, creating_udp_serverside_endpoint)
+{
+	auto ip = "::"s;
+	unsigned short port = 6969;
+	auto ep = CreateEndpoingUdp(port);
 
 	EXPECT_EQ(port, ep.port());
 	EXPECT_EQ(ip, ep.address().to_string());
