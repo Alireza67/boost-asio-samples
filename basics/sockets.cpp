1191fe8135fcd19188ffd151d8743ea15cec6860
Alireza67 <Alirezaabbasim@gmail.com>
2024-03-08 23:23:04 +0330

    [UPD] use of template method for both udp and tcp in active socket

--- basics/sockets.cpp
+++ basics/sockets.cpp
@@ -2,17 +2,17 @@
 #include <boost/asio.hpp>
 
 using namespace boost;
-
-using TcpActiveSocket = asio::ip::tcp::socket;
-using UdpActiveSocket = asio::ip::udp::socket;
 using Acceptor = asio::ip::tcp::acceptor;
 
-TcpActiveSocket CreateAndOpenActiveSocket(asio::io_service& ios)
+template<typename Protocol>
+typename Protocol::socket CreateAndOpenActiveSocket(asio::io_service& ios)
 {
-	asio::ip::tcp protocol = asio::ip::tcp::v4();
-	TcpActiveSocket socket(ios);
-	boost::system::error_code ec;
+	using Socket = typename Protocol::socket;
 
+	Protocol protocol = Protocol::v4();
+	Socket socket(ios);
+
+	boost::system::error_code ec;
 	socket.open(protocol, ec);
 
 	if (ec.value() != 0)
@@ -29,15 +29,23 @@ TcpActiveSocket CreateAndOpenActiveSocket(asio::io_service& ios)
 TEST(sockets, create_and_open_active_socket)
 {
 	asio::io_service ios;
-	EXPECT_NO_THROW(CreateAndOpenActiveSocket(ios));
+	EXPECT_NO_THROW(CreateAndOpenActiveSocket<asio::ip::tcp>(ios));
 }
 
-TcpActiveSocket CreateAndOpenActiveSocketInOneAction(asio::io_service& ios)
+TEST(sockets, create_and_open_udp_active_socket)
 {
+	asio::io_service ios;
+	EXPECT_NO_THROW(CreateAndOpenActiveSocket<asio::ip::udp>(ios));
+}
+
+template<typename Protocol>
+typename Protocol::socket CreateAndOpenActiveSocketInOneAction(asio::io_service& ios)
+{
+	using Socket = typename Protocol::socket;
 	try
 	{
-		asio::ip::tcp protocol = asio::ip::tcp::v4();
-		TcpActiveSocket socket(ios, protocol);
+		Protocol protocol = Protocol::v4();
+		Socket socket(ios, protocol);
 		return socket;
 	}
 	catch (boost::system::system_error& e) {
@@ -51,54 +59,13 @@ TcpActiveSocket CreateAndOpenActiveSocketInOneAction(asio::io_service& ios)
 TEST(sockets, create_and_open_active_socket_one_method)
 {
 	asio::io_service ios;
-	EXPECT_NO_THROW(CreateAndOpenActiveSocketInOneAction(ios));
-}
-
-UdpActiveSocket CreateAndOpenUdpActiveSocket(asio::io_service& ios)
-{
-	asio::ip::udp protocol = asio::ip::udp::v4();
-	UdpActiveSocket socket(ios);
-	boost::system::error_code ec;
-
-	socket.open(protocol, ec);
-
-	if (ec.value() != 0)
-	{
-		std::stringstream msg;
-		msg << "Failed to open the socket! Error code = "
-			<< ec.value() << ". Message: " << ec.message();
-		throw std::runtime_error(msg.str());
-	}
-
-	return std::move(socket);
-}
-
-UdpActiveSocket CreateAndOpenUdpActiveSocketInOneAction(asio::io_service& ios)
-{
-	try
-	{
-		asio::ip::udp protocol = asio::ip::udp::v4();
-		UdpActiveSocket socket(ios, protocol);
-		return socket;
-	}
-	catch (boost::system::system_error& e) {
-		std::stringstream msg;
-		msg << "Error occured! Error code = " << e.code()
-			<< ". Message: " << e.what();
-		throw std::runtime_error(msg.str());
-	}
-}
-
-TEST(sockets, create_and_open_udp_active_socket)
-{
-	asio::io_service ios;
-	EXPECT_NO_THROW(CreateAndOpenUdpActiveSocket(ios));
+	EXPECT_NO_THROW(CreateAndOpenActiveSocketInOneAction<asio::ip::tcp>(ios));
 }
 
 TEST(sockets, create_and_open_udp_active_socket_one_method)
 {
 	asio::io_service ios;
-	EXPECT_NO_THROW(CreateAndOpenUdpActiveSocketInOneAction(ios));
+	EXPECT_NO_THROW(CreateAndOpenActiveSocketInOneAction<asio::ip::udp>(ios));
 }
 
 Acceptor CreateAndOpenAcceptor(asio::io_service& ios)
