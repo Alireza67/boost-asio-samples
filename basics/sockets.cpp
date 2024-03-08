e134890fc27aa4c19a8e4f4411f2a84d65aa279e
Alireza67 <Alirezaabbasim@gmail.com>
2024-03-08 19:49:00 +0330

    [UPD] create_and_open_udp_active_socke

--- basics/sockets.cpp
+++ basics/sockets.cpp
@@ -4,6 +4,7 @@
 using namespace boost;
 
 using TcpActiveSocket = asio::ip::tcp::socket;
+using UdpActiveSocket = asio::ip::udp::socket;
 
 TcpActiveSocket CreateAndOpenActiveSocket(asio::io_service& ios)
 {
@@ -35,7 +36,7 @@ TcpActiveSocket CreateAndOpenActiveSocketInOneAction(asio::io_service& ios)
 	try
 	{
 		asio::ip::tcp protocol = asio::ip::tcp::v4();
-		asio::ip::tcp::socket socket(ios, protocol);
+		TcpActiveSocket socket(ios, protocol);
 		return socket;
 	}
 	catch (boost::system::system_error& e) {
@@ -50,4 +51,51 @@ TEST(sockets, create_and_open_active_socket_one_method)
 {
 	asio::io_service ios;
 	EXPECT_NO_THROW(CreateAndOpenActiveSocketInOneAction(ios));
+}
+
+UdpActiveSocket CreateAndOpenUdpActiveSocket(asio::io_service& ios)
+{
+	asio::ip::udp protocol = asio::ip::udp::v4();
+	UdpActiveSocket socket(ios);
+	boost::system::error_code ec;
+
+	socket.open(protocol, ec);
+
+	if (ec.value() != 0)
+	{
+		std::stringstream msg;
+		msg << "Failed to open the socket! Error code = "
+			<< ec.value() << ". Message: " << ec.message();
+		throw std::runtime_error(msg.str());
+	}
+
+	return std::move(socket);
+}
+
+UdpActiveSocket CreateAndOpenUdpActiveSocketInOneAction(asio::io_service& ios)
+{
+	try
+	{
+		asio::ip::udp protocol = asio::ip::udp::v4();
+		UdpActiveSocket socket(ios, protocol);
+		return socket;
+	}
+	catch (boost::system::system_error& e) {
+		std::stringstream msg;
+		msg << "Error occured! Error code = " << e.code()
+			<< ". Message: " << e.what();
+		throw std::runtime_error(msg.str());
+	}
+}
+
+TEST(sockets, create_and_open_udp_active_socket)
+{
+	asio::io_service ios;
+	EXPECT_NO_THROW(CreateAndOpenUdpActiveSocket(ios));
+}
+
+TEST(sockets, create_and_open_udp_active_socket_one_method)
+{
+	asio::io_service ios;
+	EXPECT_NO_THROW(CreateAndOpenUdpActiveSocketInOneAction(ios));
 }
