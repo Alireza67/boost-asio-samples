77ab9e742c14b304622c3520651412d0ed8be4c6
Alireza67 <Alirezaabbasim@gmail.com>
2024-03-09 23:40:54 +0330

    [UPD] udp binding

--- basics/binding.cpp
+++ basics/binding.cpp
@@ -29,6 +29,23 @@ Acceptor CreateAndOpenAcceptor(asio::io_service& ios, asio::ip::tcp protocol)
 	return acceptor;
 }
 
+template<typename Protocol>
+typename Protocol::socket CreateAndOpenSocket(asio::io_service& ios, Protocol protocol)
+{
+	using Socket = typename Protocol::socket;
+	try
+	{
+		Socket socket(ios, protocol);
+		return socket;
+	}
+	catch (boost::system::system_error& e) {
+		std::stringstream msg;
+		msg << "Error occured! Error code = " << e.code()
+			<< ". Message: " << e.what();
+		throw std::runtime_error(msg.str());
+	}
+}
+
 template<typename Acceptor, typename Endpoint>
 void Bind(Acceptor& acceptor, Endpoint& endpoint)
 {
@@ -57,10 +74,31 @@ TEST(binding, bind)
 TEST(binding, double_binding)
 {
 	asio::io_service ios;
-	unsigned short port = 6969;
+	unsigned short port = 7979;
 	auto endpoint = CreateEndpoint<asio::ip::tcp, asio::ip::address_v4>(port);
 	auto acceptor = CreateAndOpenAcceptor(ios, endpoint.protocol());
 	Bind(acceptor, endpoint);
 	auto acceptor2 = CreateAndOpenAcceptor(ios, endpoint.protocol());
 	EXPECT_THROW(Bind(acceptor2, endpoint), std::runtime_error);
+}
+
+TEST(binding, udp_bind)
+{
+	asio::io_service ios;
+	unsigned short port = 8989;
+	auto endpoint = CreateEndpoint<asio::ip::udp, asio::ip::address_v4>(port);
+	auto socket = CreateAndOpenSocket(ios, endpoint.protocol());
+	Bind(socket, endpoint);
+	SUCCEED();
+}
+
+TEST(binding, udp_double_bind)
+{
+	asio::io_service ios;
+	unsigned short port = 25250;
+	auto endpoint = CreateEndpoint<asio::ip::udp, asio::ip::address_v4>(port);
+	auto socket = CreateAndOpenSocket(ios, endpoint.protocol());
+	Bind(socket, endpoint);
+	auto socket2 = CreateAndOpenSocket(ios, endpoint.protocol());
+	EXPECT_THROW(Bind(socket2, endpoint), std::runtime_error);
 }
