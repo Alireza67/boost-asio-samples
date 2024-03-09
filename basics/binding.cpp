f78cc8c81d2b5cad831a43f776d88e395197016a
Alireza67 <Alirezaabbasim@gmail.com>
2024-03-10 00:15:27 +0330

    [UPD] connection without server

--- basics/binding.cpp
+++ basics/binding.cpp
@@ -2,6 +2,7 @@
 #include <boost/asio.hpp>
 
 using namespace boost;
+using namespace std::literals;
 using Acceptor = asio::ip::tcp::acceptor;
 
 template<typename Protocol, typename IpVersion>
@@ -10,6 +11,23 @@ typename Protocol::endpoint CreateEndpoint(unsigned short port)
 	using Endpoint = typename Protocol::endpoint;
 	boost::system::error_code ec;
 	asio::ip::address ip_address = IpVersion::any();
+	return Endpoint(ip_address, port);
+}
+
+template<typename Protocol>
+typename Protocol::endpoint CreateEndpoint(std::string& ip, unsigned short port)
+{
+	using Endpoint = typename Protocol::endpoint;
+	boost::system::error_code ec;
+	asio::ip::address ip_address = asio::ip::address::from_string(ip, ec);
+	if (ec.value() != 0)
+	{
+		std::stringstream msg;
+		msg << "Failed to parse the IP address. Error code = "
+			<< ec.value() << ". Message: " << ec.message();
+		throw std::runtime_error(msg.str());
+	}
+
 	return Endpoint(ip_address, port);
 }
 
@@ -61,6 +79,22 @@ void Bind(Acceptor& acceptor, Endpoint& endpoint)
 	}
 }
 
+template<typename Socket, typename Endpoint>
+void Connect(Socket& socket, Endpoint& endpoint)
+{
+	try
+	{
+		socket.connect(endpoint);
+	}
+	catch (boost::system::system_error& e)
+	{
+		std::stringstream msg;
+		msg << "Error occured! Error code = " << e.code()
+			<< ". Message: " << e.what();
+		throw std::runtime_error(msg.str());
+	}
+}
+
 TEST(binding, bind)
 {
 	asio::io_service ios;
@@ -101,4 +135,14 @@ TEST(binding, udp_double_bind)
 	Bind(socket, endpoint);
 	auto socket2 = CreateAndOpenSocket(ios, endpoint.protocol());
 	EXPECT_THROW(Bind(socket2, endpoint), std::runtime_error);
+}
+
+TEST(connecting, connect_without_server)
+{
+	asio::io_service ios;
+	unsigned short port = 54321;
+	auto ip = "127.0.0.1"s;
+	auto endpoint = CreateEndpoint<asio::ip::tcp>(ip, port);
+	auto socket = CreateAndOpenSocket(ios, endpoint.protocol());
+	EXPECT_THROW(Connect(socket, endpoint), std::runtime_error);
 }
