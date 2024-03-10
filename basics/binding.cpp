125e619c6864253963e23db1fc5eff35f9adec5c
Alireza67 <Alirezaabbasim@gmail.com>
2024-03-10 23:45:34 +0330

    [ADD] asiolib

--- basics/binding.cpp
+++ basics/binding.cpp
@@ -1,35 +1,9 @@
 #include "pch.h"
-#include <boost/asio.hpp>
+#include "asiolib/endpoints.hpp"
 
 using namespace boost;
 using namespace std::literals;
 using Acceptor = asio::ip::tcp::acceptor;
-
-template<typename Protocol, typename IpVersion>
-typename Protocol::endpoint CreateEndpoint(unsigned short port)
-{
-	using Endpoint = typename Protocol::endpoint;
-	boost::system::error_code ec;
-	asio::ip::address ip_address = IpVersion::any();
-	return Endpoint(ip_address, port);
-}
-
-template<typename Protocol>
-typename Protocol::endpoint CreateEndpoint(std::string& ip, unsigned short port)
-{
-	using Endpoint = typename Protocol::endpoint;
-	boost::system::error_code ec;
-	asio::ip::address ip_address = asio::ip::address::from_string(ip, ec);
-	if (ec.value() != 0)
-	{
-		std::stringstream msg;
-		msg << "Failed to parse the IP address. Error code = "
-			<< ec.value() << ". Message: " << ec.message();
-		throw std::runtime_error(msg.str());
-	}
-
-	return Endpoint(ip_address, port);
-}
 
 Acceptor CreateAndOpenAcceptor(asio::io_service& ios, asio::ip::tcp protocol)
 {
