013041b6e39dbc1bde733f0c435e24e466813f56
Alireza67 <Alirezaabbasim@gmail.com>
2024-03-12 21:46:04 +0330

    [UPD] streambuf

--- basics/io_buffers.cpp
+++ basics/io_buffers.cpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "pch.h"
 #include "asiolib/io_buffers.hpp"
+#include <fstream>
 
 TEST(buffers, mutable_buffers)
 {
@@ -23,4 +24,21 @@ TEST(buffers, const_buffers)
 	const std::string buffer{ "Hello Dear World!" };
 	auto asioBuffer = GetBuffer(buffer);
 	EXPECT_EQ(asioBuffer.size(), buffer.size());
+}
+
+TEST(buffers, streambuf)
+{
+	asio::streambuf buffer;
+	std::ostream output(&buffer);
+	std::istream input(&buffer);
+
+	output << "This is a simple test!\nTry to understand with more details!\n";
+
+	std::string msg;
+	std::getline(input, msg);
+	EXPECT_EQ("This is a simple test!", msg);
+	std::getline(input, msg);
+	EXPECT_EQ("Try to understand with more details!", msg);
+	std::getline(input, msg);
+	EXPECT_TRUE(msg.empty());
 }
